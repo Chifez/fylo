56b9771b4e7efee796177a2207bb8090d9dd6e63
Chifez <Chifez1@gmail.com>
2023-01-04 20:45:18 +0100

    feat:still refactoring the codebase

--- src/components/dashmain.jsx
+++ src/components/dashmain.jsx
@@ -1,46 +1,89 @@
-import React from 'react'
-import { icons } from '../data';
+import React from "react";
+import { icons } from "../data";
 
-const Dashmain = ({upload,setUpload,uploadList,setUploadList,uploadFile}) => {
+const Dashmain = ({
+	upload,
+	setUpload,
+	uploadList,
+	setUploadList,
+	uploadFile,
+	uploadProgress,
+}) => {
+	const newDate = new Date().toLocaleDateString();
+	// let date = newDate.getDate();
+	// let month = newDate.getMonth() + 1;
+	// let year = newDate.getFullYear();
 
-  const newDate = new Date().toLocaleDateString();
-  // let date = newDate.getDate();
-  // let month = newDate.getMonth() + 1;
-  // let year = newDate.getFullYear();
-  
- 
-  return (
+	const handleDelete = (idx) => {
+		uploadList.filter((item) => item.index === idx);
+	};
 
-    <div className='h-[60vh] md:h-[80vh] w-full rows-span-4 md:col-span-4 bg-white mt-10'>
-    <h3 className='text-center font-semibold text-xl text-black uppercase'>My files</h3>
-    {/* <p>{uploadList}</p> */}
-    <div className='flex flex-col justify-start items-start h-5/6 w-full px-3 border-2 border-[red] overflow-y-scroll'>
-      {uploadList === null && <div>No Files</div>}
-      {uploadList && uploadList.map((item) =>
-              <div key={Math.floor(Math.random() * 3)} className="flex flex-col justify-between m-5">
-                <div className='flex flex-col'>
-                  <div className='flex justify-around items-center'>
-                <h2>{item?.name} </h2>
-                <div className='flex justify-between' >
-                  <img className='w-5 h-5 object-cover mx-2' src={icons.download} alt="share" />
-                  <img className='w-5 h-5 object-cover mx-2' src={icons.delete} alt="delete" />
-                </div>
-                  </div>
-                <h2>resume</h2>
-                </div>
-                <div className='flex justify-between'>
-                    <p className='mr-2'>uploaded</p>
-                    <p>{newDate}</p>
-                </div>
-              </div>
-      )}
-    </div>
-  <form onSubmit={uploadFile} className='flex justify-center items-center bg-[white]'>
-      <button className='absolute bottom-20 text-center font-semibold bottom-15 text-xl px-16 rounded-md bg-btn border-2 border-transparent hover:text-black hover:bg-blue-600'><input className='opacity-0 absolute w-16' type="file" onChange={(e)=>{setUpload(e.target.files[0])}}/>Add new file</button>
-      <button type="submit" className='absolute bottom-5 left- bg-[green] my-4 px-12 rounded-md'>upload file</button>
-  </form>
-</div>
-  )
-}
+	return (
+		<div className="h-[60vh] md:h-[80vh] w-full rows-span-4 md:col-span-4 bg-white mt-10">
+			<h3 className="text-center font-semibold text-xl text-black uppercase">
+				My files
+			</h3>
+			{/* <p>{uploadList}</p> */}
+			<div className="flex flex-col justify-start items-start h-5/6 w-full px-3 border-2 border-[red] overflow-y-scroll">
+				{uploadList === null && <div>No Files</div>}
+				{uploadList &&
+					uploadList.map((item, idx) => (
+						<div key={idx} className="flex flex-col justify-between m-5">
+							<div className="flex flex-col">
+								<div className="flex justify-around items-center">
+									<h2>{item?.name} </h2>
+									<div className="flex justify-between">
+										<img
+											className="w-5 h-5 object-cover mx-2"
+											src={icons.download}
+											onClick={() => handleDelete(idx)}
+											alt="share"
+										/>
+										<img
+											className="w-5 h-5 object-cover mx-2"
+											src={icons.delete}
+											alt="delete"
+										/>
+									</div>
+								</div>
+								<h2>
+									{uploadProgress >= 100
+										? "upload complete"
+										: `uploading ${uploadProgress} %`}
+								</h2>
+							</div>
+							<div className="flex justify-between">
+								<p className="mr-2">uploaded</p>
+								<p>{newDate}</p>
+							</div>
+						</div>
+					))}
+			</div>
+			<form
+				onSubmit={uploadFile}
+				className="flex justify-center items-center bg-[white]"
+			>
+				<button className="absolute bottom-20 text-center font-semibold bottom-15 text-xl px-16 rounded-md bg-btn border-2 border-transparent hover:text-black hover:bg-blue-600">
+					<input
+						className="opacity-0 absolute w-16"
+						type="file"
+						onChange={(e) => {
+							setUpload(e.target.files[0]);
+						}}
+					/>
+					Add new file
+				</button>
+				{upload && (
+					<button
+						type="submit"
+						className="absolute bottom-5 left- bg-[green] my-4 px-12 rounded-md"
+					>
+						upload file
+					</button>
+				)}
+			</form>
+		</div>
+	);
+};
 
-export default Dashmain
+export default Dashmain;
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,99 +1,143 @@
-import React,{ useEffect,useState } from 'react'
-import { useNavigate,Routes,Route } from 'react-router-dom'
-import {storage} from '../fire.js'
-import{ ref, getDownloadURL, uploadBytesResumable } from "firebase/storage"
-import Dashmain from '../components/dashmain'
-import SideBar from '../components/SideBar'
-import Settings from './settings'
-import Updatepassword from '../components/updatepassword'
-import Changename from '../components/changename'
-import Settingspages from '../components/settingsPages'
+import React, { useEffect, useState } from "react";
+import { useNavigate, Routes, Route } from "react-router-dom";
+import { storage } from "../fire.js";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+import Dashmain from "../components/dashmain";
+import SideBar from "../components/SideBar";
+import Settings from "./settings";
+import Updatepassword from "../components/updatepassword";
+import Changename from "../components/changename";
+import Settingspages from "../components/settingsPages";
 
-const Dashboard = ({name,setName,logOut,user,email,handleAuth, setEmail, password, setPassword, 
-    error, setError, changeProfile }) => {
-    const [upload, setUpload] = useState(null)
-    const [uploadList,setUploadList] = useState([])
-    const [uploadProgress,setUploadProgress] =useState(0)
-    const [imgURL,setImgURL] = useState(null);
+const Dashboard = ({
+	name,
+	setName,
+	logOut,
+	user,
+	email,
+	handleAuth,
+	setEmail,
+	password,
+	setPassword,
+	error,
+	setError,
+	changeProfile,
+}) => {
+	const [upload, setUpload] = useState(null);
+	const [uploadList, setUploadList] = useState([]);
+	const [uploadProgress, setUploadProgress] = useState(0);
+	const [imgURL, setImgURL] = useState(null);
 
+	const uploadFile = (e) => {
+		e.preventDefault();
+		setUpload(null);
+		if (upload === null) return;
+		else {
+			// console.log(upload.name)
+			// console.log(upload)
+			const file = upload;
+			// console.log(file.name)
+			setUploadList([...uploadList, file]);
+			// console.log(file)
+			// any error that occurs would most likely be from here
 
-    const uploadFile =(e)=>{
-        e.preventDefault();
-        setUpload(null);
-        if (upload === null) return;
-        else{
-            // console.log(upload.name)
-            console.log(upload)
-            const file = upload
-            console.log(file.name)
-            setUploadList([...uploadList,file]);
-            console.log(file)
-            // any error that occurs would most likely be from here
+			const storageRef = ref(storage, `files/${file.name}`);
+			const uploadTask = uploadBytesResumable(storageRef, file);
+			uploadTask.on(
+				"state_changed",
+				(snapshot) => {
+					const progress = Math.round(
+						(snapshot.bytesTransferred / snapshot.totalBytes) * 100
+					);
+					console.log(progress);
+					setUploadProgress(progress);
+				},
+				(error) => {
+					alert(error);
+				},
+				() => {
+					getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+						setImgURL(downloadURL);
+					});
+				}
+			);
+		}
 
-            const storageRef = ref(storage,`files/${file.name}`)
-            const uploadTask = uploadBytesResumable(storageRef,file)
-            uploadTask.on('state_changed',(snapshot)=>{
-                const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
-                setUploadProgress(progress)
-            },
-            (error)=>{
-             alert(error)
-            },
-            () =>{
-                getDownloadURL(uploadTask.snapshot.ref)
-                .then((downloadURL) =>{
-                    setImgURL(downloadURL)
-                })
-            }
-         )
-        }
-       
-        // console.log([uploadList])
-        // console.log([{...uploadList?.list}])
+		// console.log([uploadList])
+		// console.log([{...uploadList?.list}])
+	};
+	const navigate = useNavigate();
 
-    }
-    const navigate = useNavigate();
+	// useEffect(() => {
+	// 	uploadFile();
+	// }, [upload]);
 
-    // useEffect(()=>{
-        
-    // },[upload])
+	useEffect(() => {
+		if (user) {
+			navigate("/dashboard");
+		}
+	}, [user]);
 
-    useEffect(() =>{
-        if(user){
-            navigate('/dashboard')
-        }
-    },[user])
-  
-  return (
-      <div className='h-screen md:grid grid-rows-5 grid-flow-row md:grid-flow-col md:grid-cols-5 md:w-screen'>
-          <Routes>
-              <Route path='/' element={<SideBar name={name}  logOut={logOut} changeProfile={changeProfile} handleAuth={handleAuth} />}>
-                    <Route index element={<Dashmain upload={upload} setUpload={setUpload} uploadList={uploadList} setUploadList={setUploadList}uploadFile={uploadFile}/>} />
-                    <Route path='/settings/*' element ={<Settings
-                      />}> 
-                      
-                        <Route index element={<Settingspages />} />
+	return (
+		<div className="h-screen md:grid grid-rows-5 grid-flow-row md:grid-flow-col md:grid-cols-5 md:w-screen">
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<SideBar
+							name={name}
+							logOut={logOut}
+							changeProfile={changeProfile}
+							handleAuth={handleAuth}
+						/>
+					}
+				>
+					<Route
+						index
+						element={
+							<Dashmain
+								upload={upload}
+								setUpload={setUpload}
+								uploadList={uploadList}
+								setUploadList={setUploadList}
+								uploadFile={uploadFile}
+								uploadProgress={uploadProgress}
+							/>
+						}
+					/>
+					<Route path="/settings/*" element={<Settings />}>
+						<Route index element={<Settingspages />} />
 
-                        <Route path='name' element={<Changename  
-                        setName={setName} 
-                        name={name}  
-                        changeProfile={changeProfile}/>} />
+						<Route
+							path="name"
+							element={
+								<Changename
+									setName={setName}
+									name={name}
+									changeProfile={changeProfile}
+								/>
+							}
+						/>
 
-                        <Route path= 'password' element={<Updatepassword
-                        email ={email}
-                        setEmail ={setEmail}
-                        password ={password}
-                        setPassword ={setPassword}
-                        error={error}
-                        setError={setError}
-                        changeProfile={changeProfile}
-                        />} />
+						<Route
+							path="password"
+							element={
+								<Updatepassword
+									email={email}
+									setEmail={setEmail}
+									password={password}
+									setPassword={setPassword}
+									error={error}
+									setError={setError}
+									changeProfile={changeProfile}
+								/>
+							}
+						/>
+					</Route>
+				</Route>
+			</Routes>
+		</div>
+	);
+};
 
-                    </Route>
-              </Route>
-          </Routes>    
-      </div>
-  )
-}
-
-export default Dashboard
+export default Dashboard;
